c7687f957e7060e60ac87cd5335511922b546da2
Ruben Chavez <chruben94@gmail.com>
2020-01-12 00:33:23 -0800

    a bunch of stuff

--- models/post.js
+++ models/post.js
@@ -1,27 +1,32 @@
 module.exports = function(sequelize, DataTypes) {
-  var Post = sequelize.define("Post", {
-    title: {
+  var Activity = sequelize.define("Activity", {
+    act: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1]
       }
     },
-    body: {
+    howto: {
       type: DataTypes.TEXT,
       allowNull: false,
       len: [1]
+    },
+    category: {
+      type: DataTypes.STRING,
+      defaultValue: "Jiu Jitsu"
     }
   });
+return Activity;
 
-  Post.associate = function(models) {
+  // Post.associate = function(models) {
    
-    Post.belongsTo(models.Activity, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
+  //   Post.belongsTo(models.Activity, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
 
-  return Post;
+  // return Post;
 };
