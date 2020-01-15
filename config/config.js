4730f69ac19435271c2cfd74874784fa224df158
Ruben Chavez <chruben94@gmail.com>
2020-01-14 21:26:53 -0800

    updates

--- config/config.js
+++ config/config.js
@@ -17,10 +17,10 @@ module.exports =
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": "password",
-    "database": "activitydeck_db",
-    "host": "127.0.0.1",
+    "username": "	i2sdzvf5o2bcqcpa",
+    "password": "	i2sdzvf5o2bcqcpa",
+    "database": "lufxfk9lb9i3e8ad",
+    "host": "nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "port": 3306,
     "dialect": "mysql"
   }
